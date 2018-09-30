6c49f1506133de891faf234ec4401954d0c77f13
Bill DuGe <billduge11@gmail.com>
2018-09-30 16:24:46 -0400

    Imported Pandas library and created dataframe to format excel output
    Creates a table with header "movies playing" and lists all elements of the array movies in order

--- showtime.py
+++ showtime.py
@@ -1,4 +1,4 @@
-
+from pandas import DataFrame
 import urllib.request
 from bs4 import BeautifulSoup
 
@@ -6,9 +6,14 @@ imdb = "https://www.imdb.com/showtimes/location?ref_=sh_lc"
 page = urllib.request.urlopen(imdb)
 soup = BeautifulSoup(page, 'html.parser')
 
+movies = []
+
 movies_list = soup.find_all('div', attrs={'class': 'hidden inline-sort-params'})
 
 for movie in movies_list:
     a = movie.find('span', attrs={'name': 'alpha'}).attrs['data-value']
-    print(a)
+    movies.append(a)
+
+df = DataFrame({'Movies Playing': movies})
+
 
