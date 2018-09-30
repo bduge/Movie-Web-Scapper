89018fb4445b254f640697656642903020ec4adb
Bill DuGe <billduge11@gmail.com>
2018-09-30 15:50:26 -0400

    Extracted names of movies currently playing

--- showtime.py
+++ showtime.py
@@ -4,4 +4,11 @@ from bs4 import BeautifulSoup
 
 imdb = "https://www.imdb.com/showtimes/location?ref_=sh_lc"
 page = urllib.request.urlopen(imdb)
-soup = BeautifulSoup(page,'html.parser')
+soup = BeautifulSoup(page, 'html.parser')
+
+movies_list = soup.find_all('div', attrs={'class': 'hidden inline-sort-params'})
+
+for movie in movies_list:
+    a = movie.find('span', attrs={'name': 'alpha'}).attrs['data-value']
+    print(a)
+
