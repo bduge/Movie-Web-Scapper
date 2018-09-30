9b9443d9b0d1bc5e012fff6510db9f8962dd998b
Bill DuGe <billduge11@gmail.com>
2018-09-30 15:20:05 -0400

    Installed and imported libraries
    Imported urllib and installed bs4 using pip

--- showtime.py
+++ showtime.py
@@ -1,1 +1,7 @@
 
+import urllib.request
+from bs4 import BeautifulSoup
+
+imdb = "https://www.imdb.com/showtimes/location?ref_=sh_lc"
+page = urllib.request.urlopen(imdb)
+soup = BeautifulSoup(page,'html.parser')
