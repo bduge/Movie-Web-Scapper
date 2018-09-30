da0c39933ea90a55ac6d9753ed34bf044a85ec7f
Bill DuGe <billduge11@gmail.com>
2018-09-30 16:47:38 -0400

    Ratings also scraped, added try and except block
    Try and except handles the error of the program being run while the excel file is open

--- showtime.py
+++ showtime.py
@@ -7,13 +7,21 @@ page = urllib.request.urlopen(imdb)
 soup = BeautifulSoup(page, 'html.parser')
 
 movies = []
+ratings = []
 
 movies_list = soup.find_all('div', attrs={'class': 'hidden inline-sort-params'})
 
 for movie in movies_list:
     a = movie.find('span', attrs={'name': 'alpha'}).attrs['data-value']
+    b = movie.find('span', attrs={'name': 'user_rating'}).attrs['data-value']
     movies.append(a)
+    ratings.append(b)
 
-df = DataFrame({'Movies Playing': movies})
 
+df = DataFrame({'Movies Playing': movies, 'Rating':ratings})
+try:
+    df.to_excel('showtimes.xlsx', sheet_name='sheet1', index=False)
+except PermissionError:
+    print("There was an error creating the spreadsheet, please make sure"
+          " the file is not currently open.")
 
