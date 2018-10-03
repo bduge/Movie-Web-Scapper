46b83adab54873cbb39a2bb11904892b805571f5
Bill DuGe <billduge11@gmail.com>
2018-10-03 18:18:29 -0400

    Scrap Release Year Added
    Wrote helper function to slice the string. Written to excel

--- showtime.py
+++ showtime.py
@@ -9,6 +9,12 @@ soup = BeautifulSoup(page, 'html.parser')
 movies = []
 ratings = []
 runtime = []
+release = []
+
+
+def get_year(s):
+    return s[:4]
+
 
 movies_list = soup.find_all('div', attrs={'class': 'hidden inline-sort-params'})
 
@@ -16,15 +22,26 @@ for movie in movies_list:
     a = movie.find('span', attrs={'name': 'alpha'}).attrs['data-value']
     b = movie.find('span', attrs={'name': 'user_rating'}).attrs['data-value']
     c = movie.find('span', attrs={'name': 'runtime'}).attrs['data-value']
+    d = movie.find('span', attrs={'name':'release_date'}).attrs['data-value']
+    year = get_year(d)
     movies.append(a)
     ratings.append(b)
     runtime.append(c)
+    release.append(year)
 
 
-df = DataFrame({'Movies Playing': movies, 'Rating': ratings, 'Runtime (min)': runtime})
+df = DataFrame({'Movies Playing': movies, 'Rating': ratings,
+                'Runtime (min)': runtime, 'Release Year': release})
 try:
     df.to_excel('showtimes.xlsx', sheet_name='sheet1', index=False)
 except PermissionError:
     print("There was an error creating the spreadsheet, please make sure"
           " the file is not currently open.")
+
+
+
+
+
+
+
 
