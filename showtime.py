a623971b5ac247d88af24958995a12b7621b1ae5
Bill DuGe <billduge11@gmail.com>
2018-10-03 18:00:34 -0400

    Scraps the runtime of the movie and writes data to excel

--- showtime.py
+++ showtime.py
@@ -8,17 +8,20 @@ soup = BeautifulSoup(page, 'html.parser')
 
 movies = []
 ratings = []
+runtime = []
 
 movies_list = soup.find_all('div', attrs={'class': 'hidden inline-sort-params'})
 
 for movie in movies_list:
     a = movie.find('span', attrs={'name': 'alpha'}).attrs['data-value']
     b = movie.find('span', attrs={'name': 'user_rating'}).attrs['data-value']
+    c = movie.find('span', attrs={'name': 'runtime'}).attrs['data-value']
     movies.append(a)
     ratings.append(b)
+    runtime.append(c)
 
 
-df = DataFrame({'Movies Playing': movies, 'Rating':ratings})
+df = DataFrame({'Movies Playing': movies, 'Rating': ratings, 'Runtime (min)': runtime})
 try:
     df.to_excel('showtimes.xlsx', sheet_name='sheet1', index=False)
 except PermissionError:
